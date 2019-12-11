755272c33bb56f5179ddbe294dcddb9a85d9b206
Etzali <etzali@icecreamarchitecture.com>
2019-12-11 16:14:35 +0000

    WIP: display custom data layer on map

--- app/javascript/app/custom_map.js
+++ app/javascript/app/custom_map.js
@@ -1,19 +1,19 @@
 import mapboxgl from "mapbox-gl";
-import * as ajax from './ajax';
+import * as ajax from "./ajax";
+
 
 const loadMap = () => {
   const mapContainer = document.getElementById("custom-map");
 
   if (mapContainer) {
-      var map = mapSetup(mapContainer);
-      map.on('load', renderPlaces(map));
-        
-      console.log(`${map} adentro de Loadmap`)
+    var map = mapSetup(mapContainer);
+    map.on("load", renderPlaces(map));
+    console.log(`${map} adentro de Loadmap`);
   }
 };
 
 const mapSetup = () => {
-    mapboxgl.accessToken =
+  mapboxgl.accessToken =
     "pk.eyJ1IjoiZXR6YWxpIiwiYSI6ImNrM2lsdHhkNjA3NWQza28xM3JqcmRhNG0ifQ.PlTOfeKllnAH4Pn9ppkZ_Q";
   return new mapboxgl.Map({
     container: "custom-map",
@@ -21,29 +21,50 @@ const mapSetup = () => {
     center: [-99.601283, 24.103338],
     zoom: 5.0
   });
-}
-
-const renderPlaces = (map) => {
-  const layerId = "states";
-  const data = "/api/data.json";
-
-  console.log(`${map} adentro de renderPlaces`);
-
-  map.addSource(layerId, {
-    type: "geojson",
-    url: data
-  });
-
-  map.addLayer(
-    {
-      id: layerId,
-      type: "circle",
-      source: data,
-      "circle-color": "turquoise"
-    }
-  );
-
 };
 
+const renderPlaces = map => {
+  function checkIfMapboxStyleIsLoaded() {
+    if (map.isStyleLoaded()) {
+      return true; // When it is safe to manipulate layers
+    } else {
+      return false; // When it is not safe to manipulate layers
+    }
+  }
+
+  function swapLayer() {
+    var check = checkIfMapboxStyleIsLoaded();
+    if (!check) {
+      // It's not safe to manipulate layers yet, so wait 200ms and then check again
+      setTimeout(function() {
+        swapLayer();
+      }, 200);
+      return;
+    }
+
+    const layerId = "states";
+    const data = "./api/data.json";
+    console.log(`${data} adentro de renderPlaces`);
+
+    map.addSource(layerId, {
+      "type": "geojson",
+      "data": data
+    });
+    console.log(`${data} despues de add source`);
+
+    map.addLayer({
+      "id": layerId,
+      "type": "circle",
+      "source": layerId,
+      'paint': {
+        'circle-radius': 6,
+        'circle-color': '#B42222'
+        }
+    });
+    console.log(`${data} despues de add layer`);
+
+  }
+  swapLayer();
+};
 
 document.addEventListener("turbolinks:load", loadMap); // need this to run javascript on rails
